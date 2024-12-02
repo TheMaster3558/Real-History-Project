67cd5b214653d748a6ebe25f427508a18138bcac
TheMaster3558 <chawk_k@outlook.com>
2024-12-01 21:29:39 -0800

    Fix type error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, AwaitedReactNode } from "react";
 import "./globals.css";
 
 
 export const metadata = {
-    title: "Louvre Website",
-    description: "Created by Kaushal Dabbiru",
+  title: "Louvre Website",
+  description: "Created by Kaushal Dabbiru",
 };
 
-export default function RootLayout(children: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined) {
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
   return (
       <html lang="en">
       <body>
