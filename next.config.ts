8b79dec5179c6c4a0306d04418258c9031621ddc
Kaushal Dabbiru <92063273+TheMaster3558@users.noreply.github.com>
2024-12-02 17:47:32 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: '/Real-History-Project',
-    assetPrefix: '/Real-History-Project/',
-    output: "export"
 };
 
 export default nextConfig;
