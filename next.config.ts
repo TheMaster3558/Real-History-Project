69997c18c6225ddcee9cc9e9caa0073ba4f0b1fb
Kaushal Dabbiru <92063273+TheMaster3558@users.noreply.github.com>
2024-12-02 17:40:58 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: "/Real-History-Project"
+    basePath: "/Real-History-Project",
+    images: {
+        loader: "akamai",
+        path: "/",
+    }
 };
 
 export default nextConfig;
